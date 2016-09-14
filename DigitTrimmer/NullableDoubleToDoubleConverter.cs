653966301a85f5b643df002eaf6cae91e11f4dd2
Johan Larsson <johan_gavle1@hotmail.com>
2016-09-14 12:17:01 +0200

    Tweak UI.

---
+++ DigitTrimmer/NullableDoubleToDoubleConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace DigitTrimmer
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class NullableDoubleToDoubleConverter : IValueConverter
+    {
+        public static readonly NullableDoubleToDoubleConverter Default = new NullableDoubleToDoubleConverter();
+
+        private NullableDoubleToDoubleConverter()
+        {
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double?)value ?? 100;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
